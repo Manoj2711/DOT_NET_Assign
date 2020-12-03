aadaac735aac3cb8ced680ba56cd536d4619b366
Manoj2711 <65231215+Manoj2711@users.noreply.github.com>
2020-12-03 19:15:29 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,178 +4,393 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment2
+namespace Assignment2._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Employee e1 = new Manager();
-            Employee e2 = new Manager();
-            Employee e3 = new Manager();
-            Console.WriteLine(e1.Empno);
-            Console.WriteLine(e2.Empno);
-            Console.WriteLine(e3.Empno);
-            Console.WriteLine("==============================");
-            Console.WriteLine(e3.Empno);
-            Console.WriteLine(e2.Empno);
-            Console.WriteLine(e1.Empno);
+            //Employee e1 = new Manager();
+            //Employee e2 = new Manager();
+            //Employee e3 = new Manager();
+            //Console.WriteLine(e1.Empno);
+            //Console.WriteLine(e2.Empno);
+            //Console.WriteLine(e3.Empno);
+            //Console.WriteLine("==============================");
+            //Console.WriteLine(e3.Empno);
+            //Console.WriteLine(e2.Empno);
+            //Console.WriteLine(e1.Empno);
+
+
+            //e1.calcNetSalary();
+
+            CEO e4 = new CEO();
+            e4.calcNetSalary();
+            e4.insert();
+            e4.update();
+            e4.delete();
+
+
+            GeneralManager e5 = new GeneralManager();
+            e5.calcNetSalary();
+            e5.insert();
+            e5.update();
+            e5.delete();
+
+
+
+            //Employee e5 = new GeneralManager();
+            //e5.calcNetSalary();
+
+
+
 
             Console.ReadLine();
+
+
+
         }
     }
 
-    public abstract class Employee
+    public interface IDBfunction
     {
+        void insert();
+        void update();
+        void delete();
+
+
+    }
+
+    public abstract class Employee:IDBfunction
+    {
+
         private string name;
         private int empNo;
-        private short deptNo;
-        private decimal basic;
+        private short DeptNo;
+        public decimal basic;
+        public abstract decimal BASIC
+        {
+            get;
+            set;
+        }
         public static int count = 0;
+
 
         public Employee()
         {
-            this.Name = "mehul";
             count++;
+            this.Name = "Manoj";
             this.Empno = count;
-            this.Deptno = 0;
-            this.basic = 25000;
+            this.DEPTNO = 10;
+            this.basic = 4500;
+
 
         }
 
-        public Employee(string name, int empNo, short deptNo, decimal basic)
+        public Employee(string name, int empNo, short DeptNo, decimal basic)
         {
             count++;
-            this.Empno = count;
             this.Name = name;
-            this.Empno = empNo;
-            this.Deptno = deptNo;
+            this.Empno = count;
+            this.DEPTNO = DeptNo;
             this.basic = basic;
+
         }
-        public string Name
+
+
+
+        public String Name
         {
+
             set
             {
                 if (value == " " || value == null)
                 {
 
-                    Console.WriteLine("name cannot be blank");
+                    Console.WriteLine("name cannot be null");
+
                 }
                 else
                 {
-                    name = value;
-                }
-            }
 
+                    name = value;
+
+                }
+
+            }
             get
             {
+
                 return name;
             }
         }
 
         public int Empno
         {
+
             private set
             {
 
                 empNo = value;
             }
-
             get
             {
+
                 return empNo;
+
             }
+
+
         }
 
-        public short Deptno
+        public short DEPTNO
         {
+
             set
             {
                 if (value > 0)
                 {
+                    DeptNo = value;
 
-                    deptNo = value;
                 }
                 else
                 {
-                    Console.WriteLine("plz..enter vlid value");
+                    Console.WriteLine("Enter DEPTNo greater than zero");
                 }
-            }
 
+            }
             get
             {
-                return deptNo;
+
+                return DeptNo;
             }
         }
 
         public abstract void calcNetSalary();
+
+
+        public void delete()
+        {
+            Console.WriteLine("EMP delete method.....");
+        }
+
+        public void insert()
+        {
+            Console.WriteLine("EMP insert method.....");
+        }
+
+        public void update()
+        {
+            Console.WriteLine("EMP update method.....");
+        }
+
     }
 
-    public class Manager : Employee
+
+    public class Manager : Employee,IDBfunction
     {
 
         private string designation;
 
+        public override decimal BASIC
+        {
+            set
+            {
+                if (value > 2000)
+                {
+                    basic = value;
+
+                }
+                else 
+                {
+                    Console.WriteLine("Basic shoud be greater than 2000");
+                
+                }
+            
+            }
+            get
+            {
+                return basic;
+            
+            }
+
+        }
 
         public Manager()
         {
-            this.Designation = designation;
+
+            this.Designation = "MANAGER";
         }
-        public Manager(string n, int e, short d, decimal b, string designation) : base(n, e, d, b)
+
+        public Manager(string n, int eNo, short DNo, decimal b, string designation) : base(n, eNo, DNo, b)
         {
             this.Designation = designation;
         }
 
         public string Designation
         {
+
             set
             {
                 if (value == " ")
                 {
-                    Console.WriteLine("can't be empty");
+                    Console.WriteLine("Can't empty ");
+
                 }
                 else
                 {
+
                     designation = value;
                 }
+
             }
 
             get
             {
                 return designation;
+
             }
-        }
+
+        }     
+
         public override void calcNetSalary()
         {
-            Console.WriteLine("this is manager netsalary method ");
+            Console.WriteLine("this is Manager netsalary method ");
+        }
+
+        public new void insert()
+        {
+            Console.WriteLine("Manager insert method..");
+        }
+
+        public new void update()
+        {
+            Console.WriteLine("Manager update method..");
+        }
+
+        public new void delete()
+        {
+            Console.WriteLine("Manager delete method..");
         }
     }
 
-    public class GeneralManager : Manager
-    {
-        private string perks;
-        public GeneralManager(string n, int e, short d, decimal b, string d1, string perks) : base(n, e, d, b, d1)
+
+    public class GeneralManager : Manager,IDBfunction
+    {  
+
+        private string Perks;
+
+        public override decimal BASIC
         {
-            this.perks = perks;
+            set
+            {
+                if (value > 2000)
+                {
+                    basic = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Basic shoud be greater than 2000");
+
+                }
+
+            }
+            get
+            {
+                return basic;
+
+            }
+
+
+
         }
+
+        public GeneralManager()
+        {
+
+            this.Perks = "XYZ";
+        }
+
+        public GeneralManager(string n, int eNo, short DNo, decimal b, string des, string Perks) : base(n, eNo, DNo, b, des)
+        {
+            this.Perks = Perks;
+        }
+
+
 
         public override void calcNetSalary()
         {
             Console.WriteLine("this is Generalmanager netsalary method ");
         }
 
+
+        public new void delete()
+        {
+            Console.WriteLine("GE delete method.....");
+        }
+
+        public new void insert()
+        {
+            Console.WriteLine("GE insert method.....");
+        }
+
+        public new void update()
+        {
+            Console.WriteLine("GE update method.....");
+        }
+
+
     }
 
-    public class CEO : Employee
+    public class CEO : Employee,IDBfunction
     {
 
-        public CEO()
+        public override decimal BASIC
         {
+            set
+            {
+                if (value > 2000)
+                {
+                    basic = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Basic shoud be greater than 2000");
+
+                }
+
+            }
+            get
+            {
+                return basic;
+
+            }
 
         }
+
+
         public sealed override void calcNetSalary()
         {
             Console.WriteLine("this is CEO netsalary method ");
         }
+
+        public new void delete()
+        {
+            Console.WriteLine("CEO delete method.....");
+        }
+
+        public new void insert()
+        {
+            Console.WriteLine("CEO insert method.....");
+        }
+
+        public new void update()
+        {
+            Console.WriteLine("CEO update method.....");
+        }
     }
+
+
 }
